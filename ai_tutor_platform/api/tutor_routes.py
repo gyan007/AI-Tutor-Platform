cffc347ec930d67789f69590df6e2347a109b5b7
Gyan Thakur <gyanthakurthakur@gmail.com>
2025-08-19 18:01:02 +0530

    Update tutor_routes.py

--- ai_tutor_platform/api/tutor_routes.py
+++ ai_tutor_platform/api/tutor_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from ai_tutor_platform.modules.tutor.chat_tutor import ask_tutor
-from ai_tutor_platform.db.mongo_client import save_chat
+from ai_tutor_platform.db.pg_client import save_chat
 
 router = APIRouter()
 
@@ -14,3 +14,4 @@ def handle_question(request: QuestionRequest):
     response = ask_tutor(request.question)
     save_chat(request.user_id, request.question, response)
     return {"response": response}
+
