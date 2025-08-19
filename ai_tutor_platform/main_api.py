13477af9427f241db5cf54f55be6b56f94d0ebba
Gyan Thakur <gyanthakurthakur@gmail.com>
2025-08-19 18:13:08 +0530

    Update main_api.py

--- ai_tutor_platform/main_api.py
+++ ai_tutor_platform/main_api.py
@@ -7,7 +7,7 @@ from ai_tutor_platform.api import (
     tracker_routes
 )
 # Update this import from mongo_client to pg_client
-# from ai_tutor_platform.db.pg_client import setup_db_schema # <-- ADDED if you want to run schema setup from API
+from ai_tutor_platform.db.pg_client import setup_db_schema # <-- ADDED if you want to run schema setup from API
 
 app = FastAPI(
     title="AI Tutoring Platform",
@@ -34,4 +34,5 @@ app.include_router(tutor_routes.router, prefix="/tutor", tags=["Tutor"])
 app.include_router(quiz_routes.router, prefix="/quiz", tags=["Quiz"])
 app.include_router(doubt_routes.router, prefix="/doubt", tags=["Doubt Solver"])
 app.include_router(tracker_routes.router, prefix="/tracker", tags=["Progress Tracker"])
+
 
